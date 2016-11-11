c929918dc679991e9ed464c024b7df1d7c1e1afd
Maxwell Santos <maxwell.santos@iterative.com.br>
2016-11-11 12:01:53 -0200

    Alura Exercises

---
+++ Operadores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                if ((i % 3 == 0) || (i % 4 == 0))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
